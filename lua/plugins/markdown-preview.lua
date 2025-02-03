5d57b97bd177b82130558c7b7fa8696fed6cc5b6
majdaltouzach <majdaltoufic88@tutanota.com>
2025-02-03 15:00:31 -0800

    Fixing things

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -4,4 +4,9 @@ return
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
     build = function() vim.fn["mkdp#util#install"]() end,
+    init = function ()
+      vim.g.mkdp_auto_start = 1
+      vim.g.mkdp_echo_preview_url = 1
+      vim.g.mkdp_browser = '/usr/bin/firefox'
+    end
 }
