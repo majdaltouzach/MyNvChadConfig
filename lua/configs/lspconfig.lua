1bdc59f237e2ab0235cdc5fb90e29f1e7ba345fd
majdaltouzach <majdaltoufic88@tutanota.com>
2025-02-03 15:42:35 -0800

    modified:   lua/configs/lspconfig.lua 	deleted:    lua/lspconfig/configs/ccls.lua

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -134,12 +134,3 @@ lspconfig.markdown_oxide.setup {
   single_file_support = {true}
 }
 
-lspconfig.ccls.setup {
-  on_attach = nvlsp.on_attach,
-  on_init = nvlsp.on_init,
-  capabilities = nvlsp.capabilities,
-  cmd = {"ccls"},
-  filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
-  -- offset_encoding = {"utf-32"},
-  single_file_support = {true}
-}
