a5ebe377eefcb6e3612d401b2184103b700cc559
majdaltouzach <majdaltoufic88@tutanota.com>
2025-02-03 14:35:54 -0800

    Configuring mappings to compile LaTex while writing

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,5 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("n", "<leader>cc", "<cmd>VimtexCompile<CR>", { desc = "Compile LaTeX document" })
-map("n", "<leader>cv", "<cmd>VimtexView<CR>", { desc = "View LaTeX document" })
+map("n", "<leader>cc", "<cmd>VimtexCompile<CR>", { desc = "Compile LaTeX document" })
+map("n", "<cmd>w", "<cmd>VimtexView<CR>", { desc = "View LaTeX document" })
