00b76a8d46391ad5aec8c53086e9ba41580c50fd
majdaltouzach <majdaltoufic88@tutanota.com>
2025-02-06 13:11:20 -0800

    Added taplo toml support

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "clangd"}
+local servers = { "html", "cssls", "clangd" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
@@ -25,14 +25,14 @@ lspconfig.hls.setup {
   on_attach = nvlsp.on_attach,
   on_init = nvlsp.on_init,
   capabilities = nvlsp.capabilities,
-  filetypes = { 'haskell', 'lhaskell', 'cabal'},
+  filetypes = { "haskell", "lhaskell", "cabal" },
 }
 
 lspconfig.jedi_language_server.setup {
   on_attach = nvlsp.on_attach,
   on_init = nvlsp.on_init,
   capabilities = nvlsp.capabilities,
-  filetypes = {"python"}
+  filetypes = { "python" },
 }
 
 lspconfig.pylsp.setup {
@@ -41,14 +41,14 @@ lspconfig.pylsp.setup {
   capabilities = nvlsp.capabilities,
   cmd = { "pylsp" },
   filetypes = { "python" },
-  single_file_support = true
+  single_file_support = true,
 }
 
 lspconfig.jdtls.setup {
   on_attach = nvlsp.on_attach,
   on_init = nvlsp.on_init,
   capabilities = nvlsp.capabilities,
-  filetypes = {"java"}
+  filetypes = { "java" },
 }
 
 lspconfig.texlab.setup {
@@ -56,7 +56,7 @@ lspconfig.texlab.setup {
   on_init = nvlsp.on_init,
   capabilities = nvlsp.capabilities,
   cmd = { "texlab" },
-  filetypes = {"tex", "plaintex", "bib"}
+  filetypes = { "tex", "plaintex", "bib" },
 }
 
 -- lspconfig.csharp_ls.setup {
@@ -120,61 +120,65 @@ lspconfig.csharp_ls.setup {
   on_attach = nvlsp.on_attach,
   on_init = nvlsp.on_init,
   capabilities = nvlsp.capabilities,
-  cmd = { "csharp-ls"},
+  cmd = { "csharp-ls" },
   filetypes = { "cs" },
-  init_options = { AutomaticWorkspaceInit = true}
+  init_options = { AutomaticWorkspaceInit = true },
 }
 
 lspconfig.markdown_oxide.setup {
   on_attach = nvlsp.on_attach,
   on_init = nvlsp.on_init,
   capabilities = nvlsp.capabilities,
-  cmd = {"markdown-oxide"},
-  filetypes = {"markdown"},
-  single_file_support = {true}
+  cmd = { "markdown-oxide" },
+  filetypes = { "markdown" },
+  single_file_support = { true },
 }
-
 
 lspconfig.arduino_language_server.setup {
   on_attach = nvlsp.on_attach,
   on_init = nvlsp.on_init,
   capabilities = {
     textDocument = {
-    semanticTokens = vim.NIL
+      semanticTokens = vim.NIL,
+    },
+    workspace = {
+      semanticTokens = vim.NIL,
+    },
   },
-  workspace = {
-    semanticTokens = vim.NIL
-  }
-  },
-  cmd = {"arduino-language-server"},
-  filetypes = {"arduino"}
+  cmd = { "arduino-language-server" },
+  filetypes = { "arduino" },
 }
 
 lspconfig.vimls.setup {
   on_attach = nvlsp.on_attach,
   on_init = nvlsp.on_init,
   capabilities = nvlsp.capabilities,
-  cmd = {"vim-language-server", "--stdio"},
-  filetypes = {"vim"},
+  cmd = { "vim-language-server", "--stdio" },
+  filetypes = { "vim" },
   init_options = {
     diagnostic = {
-    enable = true
+      enable = true,
+    },
+    indexes = {
+      count = 3,
+      gap = 100,
+      projectRootPatterns = { "runtime", "nvim", ".git", "autoload", "plugin" },
+      runtimepath = true,
+    },
+    isNeovim = true,
+    iskeyword = "@,48-57,_,192-255,-#",
+    runtimepath = "",
+    suggest = {
+      fromRuntimepath = true,
+      fromVimruntime = true,
+    },
+    vimruntime = "",
   },
-  indexes = {
-    count = 3,
-    gap = 100,
-    projectRootPatterns = { "runtime", "nvim", ".git", "autoload", "plugin" },
-    runtimepath = true
-  },
-  isNeovim = true,
-  iskeyword = "@,48-57,_,192-255,-#",
-  runtimepath = "",
-  suggest = {
-    fromRuntimepath = true,
-    fromVimruntime = true
-  },
-  vimruntime = ""
-  },
-  single_file_support = {true}
+  single_file_support = { true },
+}
 
+lspconfig.taplo.setup {
+  cmd = { "taplo", "lsp", "stdio" },
+  filetypes = { "toml" },
+  single_file_support = { true },
 }
