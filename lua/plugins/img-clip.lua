19c7a14a192ed5a5f0c373eb8efd2ffd511241b3
majdaltouzach <majdaltoufic88@tutanota.com>
2025-02-05 19:18:51 -0800

    modified:   img-clip.lua

--- lua/plugins/img-clip.lua
+++ lua/plugins/img-clip.lua
@@ -2,26 +2,26 @@ return {
   "HakonHarnes/img-clip.nvim",
   event = "VeryLazy",
   opts = {
-    use_absolute_path = false,
-    insert_mode = true,
+    default = {
+      use_absolute_path = false, -- Use relative paths to avoid LaTeX path issues
+      insert_mode_after_paste = true,
+      save_path = vim.fn.expand("%:p:h") .. "/img", -- Save images in the 'img' directory
+    },
     filetypes = {
       tex = {
-      relative_template_path = false, ---@type boolean | fun(): boolean
-      template = [[
+        relative_template_path = true, -- Ensure paths are relative for LaTeX compatibility
+        template = [[
 \begin{figure}[h]
   \centering
-  \includegraphics[width=0.8\textwidth]{$FILE_PATH}
+  \includegraphics[width=0.8\textwidth]{img/$FILE_NAME}
   \caption{$CURSOR}
   \label{fig:$LABEL}
 \end{figure}
-    ]], ---@type string | fun(context: table): string
+        ]], -- Inserts the correct LaTeX figure template
+      },
     },
-
-    }
   },
   keys = {
-    -- suggested keymap
     { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
   },
-
 }
