9944f0ed3acb4f06c778bac89e5bfe0dc517a17b
majdaltouzach <majdaltoufic88@tutanota.com>
2025-02-26 15:57:21 -0800

    Added new files for plugins.

---
+++ lua/plugins/neovim-remote-containers.lua
@@ -0,0 +1,9 @@
+return {
+  lazy = false,
+  'jamestthompson3/nvim-remote-containers',
+  cmd = {"DevcontainerOpen", "DevcontainerBuild"},
+  -- config = function ()
+  --  require("remote-containers").setup()
+  -- end
+}
+
