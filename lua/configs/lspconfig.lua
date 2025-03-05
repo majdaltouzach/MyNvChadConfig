69efc16c85751565ff31f82c50478607e4959353
majdaltouzach <majdaltoufic88@tutanota.com>
2025-03-04 20:20:52 -0800

    New JSON Language Server
    Added new json language server to lspconfig

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -158,3 +158,13 @@ lspconfig.clojure_lsp.setup {
 --   filetypes = { "gdshader", "gdshaderinc" },
 --   root_dir = {"gF"}
 -- }
+-- 
+lspconfig.jsonls.setup {
+  cmd = { "vscode-json-language-server", "--stdio" },
+  filetypes = {"json", "jsonc"},
+  init_options = {
+    provideFormatter = true
+  },
+  root_dir = {"gF"},
+  single_file_support = true
+}
