85bcb44308968e93548be8800e06e472b26a18de
majdaltouzach <majdaltoufic88@tutanota.com>
2025-03-20 21:10:03 -0700

    New cmdline

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -23,6 +23,9 @@ vim.keymap.set('n', '<leader>rc', api.connect, {})
 vim.keymap.set('n', '<leader>rd', api.disconnect, {})
 vim.keymap.set('n', '<leader>re', api.edit, {})
 
+-- FindCMDLine
+vim.api.nvim_set_keymap('n', ':', '<cmd>FineCmdLine<CR>', {noremap = true})
+
 -- (optional) Override telescope find_files and live_grep to make dynamic based on if connected to host
 local builtin = require("telescope.builtin")
 local connections = require("remote-sshfs.connections")
