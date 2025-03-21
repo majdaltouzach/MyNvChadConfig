16c86e847aca80aae23a382f5f02302c50d39f83
majdaltouzach <majdaltoufic88@tutanota.com>
2025-03-20 21:47:32 -0700

    Better leetcode.nvim config

--- lua/plugins/leetcode.lua
+++ lua/plugins/leetcode.lua
@@ -1,21 +1,45 @@
 return {
-    lazy = false,
-    "kawre/leetcode.nvim",
-    build = ":TSUpdate html", -- if you have `nvim-treesitter` installed
-    dependencies = {
-        "nvim-telescope/telescope.nvim",
-        -- "ibhagwan/fzf-lua",
-        "nvim-lua/plenary.nvim",
-        "MunifTanjim/nui.nvim",
+  "kawre/leetcode.nvim",
+  lazy = false,
+  build = ":TSUpdate html", -- Update treesitter parser for HTML
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+  },
+  opts = {
+    -- Preferred language for LeetCode problems
+    lang = "cpp",
+
+    -- Store cookie in ENV var for security
+    -- Set this in your shell config: export LEETCODE_SESSION="LEETCODE_SESSION=...; csrftoken=...; INGRESSCOOKIE=..."
+    cookie = vim.fn.getenv "LEETCODE_SESSION",
+
+    -- Optional UI customization
+    ui = {
+      theme = "dark", -- options: 'dark' | 'light' | 'auto'
+      border = "rounded", -- style: single, double, rounded, solid, shadow
     },
-    opts = {
-        lang = "cpp",  -- Set your preferred language (e.g., python, java, etc.)
-        cookie = "LEETCODE_SESSION=your_session_value; csrftoken=your_csrf_token; INGRESSCOOKIE=your_ingress_cookie;",
-        
-        -- Custom key mappings
-        mappings = {
-            next_testcase = "]t",  -- Jump to the next test case
-            prev_testcase = "[t",  -- Jump to the previous test case
-        },
+
+    -- Optional description window style
+    description = {
+      width = 0.6, -- 60% of the screen width
+      height = 0.6,
+      border = "rounded",
     },
+
+    -- Telescope configuration
+    telescope = {
+      theme = "ivy", -- options: dropdown, cursor, ivy
+    },
+
+    -- Custom key mappings
+    mappings = {
+      next_testcase = "<leader>ln", -- "leetcode next"
+      prev_testcase = "<leader>lp", -- "leetcode previous"
+      pick_question = "<leader>lq", -- "leetcode question"
+      run_code = "<leader>lr", -- "leetcode run"
+      submit_code = "<leader>ls", -- "leetcode submit"
+    },
+  },
 }
