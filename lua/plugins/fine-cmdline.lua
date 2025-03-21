77798a7ad9e9d114187f135ead3b62bc9387652a
majdaltouzach <majdaltoufic88@tutanota.com>
2025-03-20 20:39:15 -0700

    Fixed dependencies

--- lua/plugins/fine-cmdline.lua
+++ lua/plugins/fine-cmdline.lua
@@ -1,14 +1,11 @@
 return {
   lazy = false,
   'VonHeikemen/fine-cmdline.nvim',
+  dependencies = {'MunifTanjim/nui.nvim'},
   config = function ()
     require('fine-cmdline').setup({
 
     })
   end,
-  dependencies = {
-    lazy = false,
-    'MunifTanjim/nui.nvim'
-  }
 
 }
