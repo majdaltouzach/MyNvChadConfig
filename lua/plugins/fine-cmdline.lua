d74454418141e290cbd1ccd1f8f49b2331049920
majdaltouzach <majdaltoufic88@tutanota.com>
2025-03-20 21:38:02 -0700

    New config in fine-cmdline

--- lua/plugins/fine-cmdline.lua
+++ lua/plugins/fine-cmdline.lua
@@ -1,11 +1,37 @@
 return {
-  lazy = false,
   'VonHeikemen/fine-cmdline.nvim',
-  dependencies = {'MunifTanjim/nui.nvim'},
-  config = function ()
+  lazy = false,
+  dependencies = { 'MunifTanjim/nui.nvim' },
+  config = function()
     require('fine-cmdline').setup({
-
+      cmdline = {
+        enable_keymaps = true,
+        smart_history = true,
+        prompt = '➤ ', -- Looks clean and modern
+      },
+      popup = {
+        position = {
+          row = '40%', -- vertically centered-ish
+          col = '50%', -- horizontally centered
+        },
+        size = {
+          width = '60%',
+        },
+        border = {
+          style = 'rounded',
+        },
+        win_options = {
+          winhighlight = 'Normal:Normal,FloatBorder:FloatBorder',
+        },
+      },
+      hooks = {
+        after_mount = function(input)
+          -- Autofocus the input field
+          input:map('i', '<C-c>', function()
+            input:unmount()
+          end, { noremap = true })
+        end
+      }
     })
   end,
-
 }
