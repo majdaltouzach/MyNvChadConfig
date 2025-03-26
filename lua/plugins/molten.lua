d4aa94933f1413c389718375e12ae03e1baa70ae
majdaltouzach <majdaltoufic88@tutanota.com>
2025-03-26 16:47:24 -0700

    Added commands to molten plugin

--- lua/plugins/molten.lua
+++ lua/plugins/molten.lua
@@ -7,4 +7,8 @@ return {
     -- this is an example, not a default. Please see the readme for more configuration options
     vim.g.molten_output_win_max_height = 12
   end,
+  cmds = {
+    "MoltenInit",
+    "MoltenEvaluateLine"
+  }
 }
