388e6d7acfadf41adb2a1655b3fd2817ffd18caf
majdaltouzach <majdaltoufic88@tutanota.com>
2025-04-10 11:36:54 -0700

    Enhanced flutter tools

--- lua/plugins/flutter-tools.lua
+++ lua/plugins/flutter-tools.lua
@@ -1,15 +1,42 @@
-return
-{
-    'nvim-flutter/flutter-tools.nvim',
-    lazy = false,
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        'stevearc/dressing.nvim', -- optional for vim.ui.select
-    },
-    config = function ()
-      require("flutter-tools").setup{
+return {
+  "nvim-flutter/flutter-tools.nvim",
+  lazy = false,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "stevearc/dressing.nvim", -- optional for vim.ui.select
+  },
+  config = function()
+    require("flutter-tools").setup {
+      flutter_path = "flutter", -- or full path to flutter if needed
+      widget_guides = {
+        enabled = true,
+      },
+      closing_tags = {
+        highlight = "Comment", -- Highlight for the closing tag
+        prefix = "// ",
+        enabled = true,
+      },
+      dev_log = {
+        enabled = true,
+        open_cmd = "tabedit", -- "tabedit", "vsplit", "split"
+      },
+      lsp = {
+        color = {
+          enabled = true,
+        },
+        on_attach = function(client, bufnr)
+          local bufmap = function(mode, lhs, rhs)
+            vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, { noremap = true, silent = true })
+          end
 
-      }
-    end,
-    ft = {"dart"}
+          bufmap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+          bufmap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
+          bufmap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
+          bufmap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
+        end,
+      },
+      root_patterns = { "pubspec.yaml" },
+    }
+  end,
+  ft = { "dart" },
 }
