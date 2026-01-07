d33235feffc0927c6e848d4dc52e0b7627d42cf4
majdaltouzach <majdaltoufic88@tutanota.com>
2026-01-07 12:51:27 -0800

    Fixed typst-vim

--- lua/plugins/typst-vim.lua
+++ lua/plugins/typst-vim.lua
@@ -1,5 +1,21 @@
 return {
-  'kaarmu/typst.vim',
-  ft = 'typst',
-  lazy=false,
+  {
+    'kaarmu/typst.vim',
+    ft = 'typst',
+    lazy = false,
+  },
+
+  {
+    'chomosuke/typst-preview.nvim',
+    lazy = false,
+    version = '1.*',
+    build = function()
+      require('typst-preview').update()
+    end,
+    config = function()
+      require('typst-preview').setup({
+        open_cmd = 'mupdf %s',
+      })
+    end,
+  }
 }
